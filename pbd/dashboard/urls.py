0ee0ee74b5b80b06002f88b34b381ed57d0483cc
Aji nur cahyo <99330937+ajinurcahyo@users.noreply.github.com>
2023-11-21 20:46:52 +0700

    Delete pbd directory

--- pbd/dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.index1, name='index1'),
-    path('data2', views.index2, name='index2'),
-    path('data3', views.index3, name='index3'),
-]
